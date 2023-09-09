bf6a70ff86eec881233d837e92d8ac3f5c77e5f1
code9633 <shashikaudara5566@gmail.com>
2023-09-09 21:15:17 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".mytext", {
+    strings :  ["Python Developer", "Python Back-end Developer", "AI/ML Engineer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+
